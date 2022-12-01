ae852a7c2ca59ee9dac4fc550e0bad5f60911c67
A <A@DESKTOP-N125OB8.local>
2022-12-01 14:23:52 +0100

    build in gateway server

---
+++ gateway/src/main/java/hu/webuni/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package hu.webuni.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+//ettől lesz ez egy discovery kliens -> ezért fogja a discovery szervertől lekérdezni, hogy meliyk api, melyik porton fut
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
